5413ace5878c473f2f42a19274d0614e479f83e6
Vic <victormaina05@gmail.com>
2025-10-17 14:50:46 +0300

    update admin.py

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(BaseUserAdmin):
+    """Custom admin panel for the CustomUser model."""
+
+    # Fields shown in the admin list view
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_active",
+        "verified",
+        "is_staff",
+        "date_joined",
+    )
+    list_filter = ("role", "verified", "is_active", "is_staff", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("-date_joined",)
+    readonly_fields = ("date_joined", "last_login")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (_("Permissions"), {
+            "fields": (
+                "role",
+                "verified",
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "role",
+                    "is_active",
+                    "is_staff",
+                    "verified",
+                ),
+            },
+        ),
+    )
+
+    # Make email the main login identifier in the admin
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
+
+    def get_readonly_fields(self, request, obj=None):
+        """Prevent non-superadmins from editing critical fields."""
+        if not request.user.is_superuser:
+            return self.readonly_fields + ("role", "is_superuser")
+        return self.readonly_fields
