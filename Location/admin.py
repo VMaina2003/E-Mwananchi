eceadd7fbeee47dc05717fae616d222c6c418b48
Vic <victormaina05@gmail.com>
2025-10-18 12:21:16 +0300

    add ward admin under admin.py

--- Location/admin.py
+++ Location/admin.py
@@ -30,4 +30,32 @@ class CountyAdmin(admin.ModelAdmin):
     search_fields = ("name", "capital")
     ordering = ("name",)
     inlines = [SubCountyInline]
-    
+
+
+# ============================================================
+#   SUBCOUNTY ADMIN
+# ============================================================
+
+@admin.register(SubCounty)
+class SubCountyAdmin(admin.ModelAdmin):
+    list_display = ("name", "county", "code", "created_at")
+    list_filter = ("county",)
+    search_fields = ("name", "county__name")
+    ordering = ("county__name", "name")
+    inlines = [WardInline]
+
+
+# ============================================================
+#   WARD ADMIN
+# ============================================================
+
+@admin.register(Ward)
+class WardAdmin(admin.ModelAdmin):
+    list_display = ("name", "subcounty", "county_name", "latitude", "longitude")
+    list_filter = ("subcounty__county", "subcounty")
+    search_fields = ("name", "subcounty__name", "subcounty__county__name")
+    ordering = ("subcounty__county__name", "subcounty__name", "name")
+
+    def county_name(self, obj):
+        return obj.subcounty.county.name
+    county_name.short_description = "County"
