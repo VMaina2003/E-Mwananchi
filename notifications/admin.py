0be91e783ea569ad4e5a4bde9cc92abbef2a7349
Vic <victormaina05@gmail.com>
2025-10-20 20:08:57 +0300

    update admin.py

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "recipient",
+        "actor",
+        "verb",
+        "is_read",
+        "created_at",
+    )
+    list_filter = ("is_read", "created_at")
+    search_fields = ("verb", "recipient__email", "actor__email")
+    ordering = ("-created_at",)
